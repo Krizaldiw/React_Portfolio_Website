a808a7e8aa423808efe9ab670c9160ced5edf5a9
Khalif Rizaldi <48214611+Krizaldiw@users.noreply.github.com>
2024-05-08 16:55:45 +0700

    [RFC 0805202404] Update Hero.jsx

--- src/components/UI/Hero.jsx
+++ src/components/UI/Hero.jsx
@@ -48,7 +48,7 @@ const Hero = () => {
                 software development with 1 - 2 years experiences. Skilled in
                 test case creation, management, execution, and reporting for
                 Mobile and Web Applications my skill include but are not limited
-                to Web Development and Data Analysis with ongoing commitment to
+                to Web Development and Software Engineering with ongoing commitment to
                 professional growth.
               </p>
             </div>
