8ebcd5904cf2d9c5e4b9ec83f641dc8c96bbb91f
Khalif Rizaldi <48214611+Krizaldiw@users.noreply.github.com>
2024-05-08 16:45:17 +0700

    [RFC 0805202402] Update Experiences.jsx

--- src/components/UI/Experiences.jsx
+++ src/components/UI/Experiences.jsx
@@ -21,6 +21,39 @@ const Experiences = () => {
               <div className="relative text-gray-700 antialised text-sm font font-semibold">
                 {/* Vertical Line Running Through The Middle */}
                 <div className="hidden absolute w-1 sm:block bg-indigo-300 h-full left-1/2 transform -translate-x-1/2"></div>
+
+                {/* Right Card */}
+                <div className="mt-6 sm:mt-0 sm:mb-12">
+                  <div className="flex items-center flex-col sm:flex-row">
+                    <div className="flex justify-end w-full mx-auto items-center">
+                      <div className="w-full sm:w-1/2 sm:pl-8">
+                        <div
+                          data-aos="fade-right"
+                          data-aos-duration="1200"
+                          className="bg-white p-4 rounded shadow group hover:bg-primaryColor cursor-pointer ease-in duration-150 mx-auto sm:mx-0" // Tambahkan kelas "mx-auto" di sini
+                        >
+                          <h5 className="text-primaryColor font-[600] mb-3 group-hover:text-white group-hover:font-[600] text-2xl">
+                            Research Assistant
+                          </h5>
+                          <p className="text-[15px] text-smallTextColor group-hover:text-white group-hover:font-[500] leading-7">
+                            September 2021 - July 2022
+                            Teknik Informatika (UMS)
+                            Surakarta, Indonesia
+                          </p><br />
+                          <p className="text-[15px] text-smallTextColor group-hover:text-white group-hover:font-[500] leading-7">
+                            Investigated and Analysis the most frequently discussed keywords React JavaScript library. Collaborate with Ph.D Students at Nara Institute Of Science and Technology (Japan) 
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="rounded-full bg-primaryColor border-white border-4 w-10 h-10 absolute left-1/2 transform -translate-x-1/2 -translate-y-4 sm:translate-y-0 flex items-center justify-center">
+                        <figure>
+                            <img src={backendImg} alt="" />
+                        </figure>
+                    </div>
+                  </div>
+                </div>
+                
                 {/* Left Card */}
                 <div className="mt-6 sm:mt-0 sm:mb-12">
                   <div className="flex items-center flex-col sm:flex-row">
